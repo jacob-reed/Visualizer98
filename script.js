ded8c98338f951b22b26df520b3d43b7c62c9251
Jacob R <iamjacobreed@gmail.com>
2018-03-13 19:33:55 -0700

    Move windows

--- script.js
+++ script.js
@@ -258,38 +258,44 @@ function trackTime() {
 // icons
 function showWindow1() {
   document.getElementById('window1').style.visibility = "visible";
-  document.getElementById('window1top').style.visibility = "visible";
+  document.getElementById('window1header').style.visibility = "visible";
+  document.getElementById('window1content').style.visibility = "visible";
   document.getElementById('windowhelp').style.display = "inline-block";
 }
 
 function showWindow2() {
   document.getElementById('window2').style.visibility = "visible";
-  document.getElementById('window2top').style.visibility = "visible";
+  document.getElementById('window2header').style.visibility = "visible";
+  document.getElementById('window2content').style.visibility = "visible";
   document.getElementById('windowabout').style.display = "inline-block";
 }
 
 function showWindow3() {
   document.getElementById('window3').style.visibility = "visible";
-  document.getElementById('window3top').style.visibility = "visible";
+  document.getElementById('window3header').style.visibility = "visible";
+  document.getElementById('window3content').style.visibility = "visible";
   document.getElementById('windowthemes').style.display = "inline-block";
 }
 
 // close buttons
 function closeWindow1() {
   document.getElementById('window1').style.visibility = "hidden";
-  document.getElementById('window1top').style.visibility = "hidden";
+  document.getElementById('window1header').style.visibility = "hidden";
+  document.getElementById('window1content').style.visibility = "hidden";
   document.getElementById('windowhelp').style.display = "none";
 }
 
 function closeWindow2() {
   document.getElementById('window2').style.visibility = "hidden";
-  document.getElementById('window2top').style.visibility = "hidden";
+  document.getElementById('window2header').style.visibility = "hidden";
+  document.getElementById('window2content').style.visibility = "hidden";
   document.getElementById('windowabout').style.display = "none";
 }
 
 function closeWindow3() {
   document.getElementById('window3').style.visibility = "hidden";
-  document.getElementById('window3top').style.visibility = "hidden";
+  document.getElementById('window3header').style.visibility = "hidden";
+  document.getElementById('window3content').style.visibility = "hidden";
   document.getElementById('windowthemes').style.display = "none";
 }
 
@@ -347,6 +353,132 @@ function dragElement(elmnt) {
 
 // Visualizer Settings
 dragElement(document.getElementById(("settings")));
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    /* if present, the header is where you move the DIV from:*/
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    /* otherwise, move the DIV from anywhere inside the DIV:*/
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    /* stop moving when mouse button is released:*/
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
+// Window 1 (Help)
+dragElement(document.getElementById(("window1")));
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    /* if present, the header is where you move the DIV from:*/
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    /* otherwise, move the DIV from anywhere inside the DIV:*/
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    /* stop moving when mouse button is released:*/
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
+// Window 2 (About)
+dragElement(document.getElementById(("window2")));
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    /* if present, the header is where you move the DIV from:*/
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    /* otherwise, move the DIV from anywhere inside the DIV:*/
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    /* stop moving when mouse button is released:*/
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
+// Window 3 (Themes)
+dragElement(document.getElementById(("window3")));
 
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
